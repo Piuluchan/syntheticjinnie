dda52ad45a568cce59aa3e25758fc25c80d7a98a
Anthea <plu@192.168.0.103>
2021-01-30 11:31:25 +0800

    project amended

--- src/project.js
+++ src/project.js
@@ -1190,7 +1190,6 @@ window.__require = function e(t, n, o) {
               // 物理引擎
               n.getComponent(cc.RigidBody).type = cc.RigidBodyType.Static,
               n.getComponent(cc.PhysicsCircleCollider).radius = 0,
-              n.getComponent(cc.PhysicsCircleCollider).restitution = 0.3,
               n.getComponent(cc.PhysicsCircleCollider).apply(), cc.tween(n).to(.5, {
               scale: 1
             }, {
